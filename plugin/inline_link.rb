118c578cdce5473ec6bcf60241551913a5d59579
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2008-04-30 01:26:06 +0000

    convert to utf-8
    git-svn-id: file:///tmp/repos/svn/pakiwiki/trunk/pakiwiki@72 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- plugin/inline_link.rb
+++ plugin/inline_link.rb
@@ -2,13 +2,13 @@ def inline_link(element)
   page = element.attr["page"]
   page= element.innerYATML if !page
   if(page =~ /\A(http|https|ftp|skype|callto):[A-Za-z0-9:\/?#\[\]@~$&'()*+,;=%._^\-]*\Z/m)
-    # FIXME: $B%j%s%/Fb$G%?%0;H$&$K$O(B?
+    # FIXME: リンク内でタグ使うには?
     #return ["a",{"href"=>page}] + convert_element(element.contents) + [["sub",{},["small",{},"[out link]"]]]
     return ["a",{"href"=>page,"rel"=>"nofollow"},element.innerYATML,["sub",{},["small",{},"[out link]"]]]
   end
   action = "show"
   action = element.attr["action"].to_s.gsub(/[^A-Za-z0-9.]/,"") if element.attr.has_key?("action")
-  # FIXME: $B%j%s%/Fb$G%?%0;H$&$K$O(B?
+  # FIXME: リンク内でタグ使うには?
   #return ["a",{"href"=>$wiki.make_link(page,action)}] + convert_element(element.contents)
   if($storage.get_page(page).last_snapshot() != nil)
     return ["a",{"href"=>$wiki.make_link(page,action)},element.innerYATML]
