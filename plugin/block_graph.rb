1cc002f6b77e33be0e4c747f6a83d8df694a12d3
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2008-06-17 02:54:09 +0000

    make link to block_graph.rb
    git-svn-id: file:///tmp/repos/svn/pakiwiki/trunk/pakiwiki@93 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- plugin/block_graph.rb
+++ plugin/block_graph.rb
@@ -1,16 +1,131 @@
+require "rubygems"
+require "rparsec"
+
+eval <<'EOL', TOPLEVEL_BINDING
+include RParsec
+EOL
+
+class DotParser
+  include Parsers
+  # lexer
+  def delim
+    (whitespace| char("\n") | char("\r") | comment_block("/*","*/")).many_ 
+  end
+  def id
+    regexp(/[a-zA-Z_][a-zA-Z0-9_]*/) | regexp(/-?(\.[0-9]+|[0-9]+(\.[0-9]*)?)/) | 
+      (char("\"") >> (not_char(?")|str("\\\"")).many_.fragment << char("\"")).map do |raw|
+         raw.gsub("\\\"","\"")
+      end
+  end
+
+  def edgeop
+    string("--") | string("->")
+  end
+
+  # parser
+  def dot
+    stmt_list = stmt = subgraph = attr_stmt = attr_list = a_list = edge_stmt = 
+      edgeRHS = node_stmt = node_id = port = compass_pt = nil
+#graph		:  	[ strict ] (graph | digraph) [ ID ] '{' stmt_list '}'
+    body = delim >> char("{") >> lazy{stmt_list} << char("}")
+    head = string("strict ").optional() >> delim >> 
+      ( string("digraph") | string("graph") ) >> delim>> id 
+    graph = delim >> head >> body << delim << eof
+#stmt_list 	: 	[ stmt [ ';' ] [ stmt_list ] ]
+    stmt_list = delim >> sequence(lazy{stmt},char(";").optional,lazy{stmt_list}) do |stm,b,arr|
+      if(arr == nil)
+        if(stm == nil)
+          []
+        else
+          [stm]
+        end
+      else
+        if(stm == nil)
+          arr
+        else
+          arr << stm
+        end
+      end
+    end.optional
+#stmt 		: 	node_stmt
+#		| 	edge_stmt
+#		| 	attr_stmt
+#		| 	ID '=' ID
+#		| 	subgraph
+    eq_stmt = sequence(id,delim >> char("=") << delim,id) do |i,b,i2|
+      [i,i2]
+    end >> value(nil)
+    stmt = delim >> (eq_stmt | lazy{node_stmt} | lazy{edge_stmt} | lazy{attr_stmt} | lazy{subgraph}) << delim
+#attr_stmt 	: 	(graph | node | edge) attr_list
+    attr_stmt = (string("graph") | string("node") | string("edge") ) >> lazy{attr_list} >> value(nil)
+#attr_list 	: 	'[' [ a_list ] ']' [ attr_list ]
+    attr_list = delim >> char('[') >> lazy{a_list}.optional << 
+                delim << char(']') << delim << lazy{attr_list}.optional
+#a_list	 	: 	ID [ '=' ID ] [ ',' ] [ a_list ]
+    a_list = delim >> id << delim << (char('=') >> delim >> id ).optional >> 
+             delim >> char(',').optional >> lazy{a_list}.optional
+#edge_stmt 	: 	(node_id | subgraph) edgeRHS [ attr_list ]
+    edge_stmt = sequence((lazy{node_id} | lazy{subgraph}),lazy{edgeRHS}) do|i,arr|
+                  arr << i
+                end << attr_list.optional
+#edgeRHS 	: 	edgeop (node_id | subgraph) [ edgeRHS ]
+    edgeRHS = delim >> edgeop >> delim >> sequence( (lazy{node_id} | lazy{subgraph}),delim,lazy{edgeRHS}.optional) do |i,b,arr|
+                if(arr == nil)
+                  [i]
+                else
+                  arr << i
+                end
+              end
+#node_stmt 	: 	node_id [ attr_list ]
+    node_stmt = lazy{node_id} << attr_list.optional
+#node_id 	: 	ID [ port ]
+    node_id = id << lazy{port.optional}
+#port 		: 	':' ID [ ':' compass_pt ]
+#		| 	':' compass_pt
+    port = (char(':') >> id << (char(':') >> lazy{compass_pt}).optional) | (char(':') >> lazy{compass_pt})
+#subgraph 	: 	[ subgraph [ ID ] ] '{' stmt_list '}'
+    subgraph = (string("subgraph") >> delim >> id.optional ).optional >> delim >> char('{') >> stmt_list  << char("}")
+#compass_pt 	: 	(n | ne | e | se | s | sw | w | nw)
+    compass_pt = (string("n") | string("ne") | string("e") | string("se") | 
+                  string("s") | string("sw") | string("w") | string("nw"))
+
+    graph
+  end
+
+
+  def self.parser
+    DotParser.new.dot
+  end
+end
+
 require 'digest/sha1'
 
 def block_graph(element)
   dot = element.innerYATML
-  name = Digest::SHA1.hexdigest(dot) + ".png"
-  fname = "./plugin/graph/"+name
-  uname = $wiki.cgibase + "/plugin/graph/"+name
-  if(!File.exist?(fname))
+STDERR.puts dot
+#  begin 
+    links = DotParser.parser.parse(dot).flatten.uniq.sort.map{|i| "\"#{i.gsub("\"","\\\"")}\" [ URL = \"#{ $wiki.make_link(i) }\"]; " }.join()
+    dot = dot.gsub("{","{#{links}") 
+#  rescue 
+#  end
+STDERR.puts dot
+  name = Digest::SHA1.hexdigest(dot)
+  fname = "./plugin/graph/"+name + ".png"
+  fhname = fname + ".html"
+  uname = $wiki.cgibase + "/plugin/graph/"+name + ".png"
+  if(!File.exist?(fname) || !File.exist?(fhname))
     IO.popen("dot -Nfontname='/usr/local/share/fonts/TrueType/ipag.ttf' -Tpng -o #{fname}","w") do |w|
       w.puts dot
     end
+    IO.popen("dot -Tcmapx -o #{fhname}","w") do |w|
+      w.puts dot
+    end
   end
-  return ["div",{},["img",{"src"=>uname}]]
-end
+  areas = []
+  File.read(fhname).gsub(/<area shape="poly" href="([^"]+)" title="([^"]+)" alt="" coords="([^"]+)"\/>/){areas << [$1,$2,$3]}
+  area = areas.map{|i| ["area",{"shape"=>"poly", "href"=>i[0], "title"=>i[1], "coords"=>i[2]}] }
 
-# vim: sw=2 : ts=1000 :
+  return ["div",{},["img",{"src"=>uname,"usemap"=>"##{name}"}],
+           ["map",{"id"=>name,"name"=>name}] + area]
+end
+# vim: sw=2 : ts=8 :
