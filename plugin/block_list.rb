0b3690a3a7f6c796a6f36e1392d265e60b112359
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2008-07-09 10:19:47 +0000

    support nested list
    git-svn-id: file:///tmp/repos/svn/pakiwiki/trunk/pakiwiki@109 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- plugin/block_list.rb
+++ plugin/block_list.rb
@@ -1,5 +1,40 @@
 def block_list(element)
-  return ["ul",{}] + element.innerYATML.split(/\r?\n/).map {|i| ["li",{}]+convertYatml2Wabisabi(i) }
+  indent = []
+  items = []
+  current = []
+  element.innerYATML.split(/\r?\n/).each do|line|
+    if indent.size != 0
+      while line[0 .. (indent.join.size-1)] != indent.join
+        indent.pop
+        c = items.pop
+        if(c.size != 0)
+          c[-1] << ["ul",{}] + current
+        else
+          c << ["li",{},["ul",{}] + current]
+        end
+        current = c
+        break if indent.size == 0
+      end
+      line = line[indent.join.size .. -1 ]
+    end
+    if(line =~ /^( +)/)
+      items.push current
+      current = []
+      line.sub!(/^( +)/,"")
+      indent.push $1
+    end
+    current.push( ["li",{}] + convertYatml2Wabisabi(line) )
+  end
+  while items.size != 0
+    c = items.pop
+    if(c.size != 0)
+      c[-1] << ["ul",{}] + current
+    else
+      c << ["li",{},["ul",{}] + current]
+    end
+    current = c
+  end
+  return ["ul",{}] + current
 end
 
 # vim: sw=2 : ts=1000 :
