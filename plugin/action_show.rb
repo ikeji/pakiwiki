c946f1b46a5691674b3ef647cc459f613d6bf58c
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2008-07-29 12:45:25 +0000

    add revert button
    git-svn-id: file:///tmp/repos/svn/pakiwiki/trunk/pakiwiki@112 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- plugin/action_show.rb
+++ plugin/action_show.rb
@@ -1,10 +1,29 @@
 def action_show()
   page = $storage.get_page($wiki.page)
   snapshot = page.last_snapshot()
+  alert = ""
+  aptitle = ""
   if($wiki.time != nil)
     for s in page.snapshot_list.sort{|a,b| b.time <=> a.time }
       break if s.time < $wiki.time
       snapshot = s
+      alert = WabisabiConverter.toHTML([
+       ["div",{"class"=>"alert"},
+         "This page is old version(#{s.time.to_s})",
+         ["form",{ "action"=>$wiki.make_link($wiki.page,"update"),
+                   "method"=>"post",
+                   "name"=>"edit",
+                 },
+           ["input",{"type"=>"hidden",
+                     "name"=>"msg",
+                     "value"=>s.data,
+                    }],
+           "You can ",
+           ["input",{"type"=>"submit","value"=>"revert"}],
+           " To this version.",
+         ]
+       ]])
+      aptitle = " (#{s.time.to_s})"
     end
   end
 
@@ -15,7 +34,7 @@ def action_show()
     return nil
   end
 
-  return :body => convert(snapshot.data), :title => $wiki.page;
+  return :body => (alert + convert(snapshot.data)), :title => ($wiki.page + aptitle)
 end
 
 # vim: sw=2 : ts=1000 :
