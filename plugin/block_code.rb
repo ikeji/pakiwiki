58982f3673e74ebe90768ef341b1f715e0994eb6
ikeji <ikeji@f8368ae4-1e99-dc11-9799-0018f35ab5fa>
2009-09-05 05:30:38 +0000

    Add code plugin.
    git-svn-id: file:///tmp/repos/svn/pakiwiki/trunk/pakiwiki@259 f8368ae4-1e99-dc11-9799-0018f35ab5fa

--- plugin/block_code.rb
+++ plugin/block_code.rb
@@ -1,4 +1,7 @@
 def block_code(element)
+  if element.attr.has_key?("filename")
+    return ["div",{"class"=>"code"}, ["div",{"class"=>"filename"}, element.attr["filename"].to_s], ["pre",{},element.innerYATML.to_s]]
+  end
   return ["pre",{},element.innerYATML]
 end
 
