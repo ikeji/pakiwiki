c089ba9c559601cc8fb976f9e242c487bf222dbe
Shun Ikejima <syun@ikejima.org>
2014-07-02 01:01:14 -0700

    Use wabisabi to aboid space/newline.

--- plugin/action_backup.rb
+++ plugin/action_backup.rb
@@ -4,21 +4,19 @@ require "cgi"
 def action_backup()
   page = $storage.get_page($wiki.page)
   list = page.sorted_snapshot_list.map do |i|
-    <<EOL 
-<li>
-  <a href="#{$wiki.make_link($wiki.page + "@" + i.time.iso8601)}">
-    #{i.time.rfc822}
-  </a>
- - 
-  <a href="#{$wiki.make_link($wiki.page + "@" + i.time.iso8601,"srcdiff")}">
-    diff
-  </a>
-</li>
-EOL
-  end.join()
+    ["li",{},
+       ["a", { "href" => $wiki.make_link($wiki.page + "@" + i.time.iso8601) },
+         i.time.rfc822
+       ],
+       "-",
+       ["a", { "href" => $wiki.make_link($wiki.page + "@" + i.time.iso8601, "srcdiff") },
+         "diff"
+       ]
+    ]
+  end
 
   return :title => CGI.escapeHTML($wiki.page) + " - 過去の編集一覧" ,
-    :body => "<ul>" + list + "</ul>"
+    :body => WabisabiConverter.toHTML([["ul",{}] + list])
 end
 
 # vim: sw=2 : ts=1000 :
