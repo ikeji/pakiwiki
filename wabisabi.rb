aba7fb1f0f808ad46d6eecb71f22c401cd924949
Shun Ikejima <syun@ikejima.org>
2014-07-02 00:59:44 -0700

    small refactoring wabisabi.rb

--- wabisabi.rb
+++ wabisabi.rb
@@ -3,18 +3,22 @@
 class WabisabiConverter
   def self.toHTML(wabisabi)
     return wabisabi.map do |e|
-      if(e.class == String)
-        CGI.escapeHTML(e)
-      else
+      if(e.class == Array)
         "<" + CGI.escapeHTML(e[0]) + 
-          e[1].keys.map do |k|
-            " " + CGI.escapeHTML(k) + "=\"" + CGI.escapeHTML(e[1][k]) + "\""
-          end.join("") + 
+          if(e.size>=2)
+            e[1].keys.map do |k|
+              " " + CGI.escapeHTML(k) + "=\"" + CGI.escapeHTML(e[1][k]) + "\""
+            end.join("")
+          else
+            ""
+          end + 
           if(e.size==2 && e[0] != "div") # HACK: Don't close div tag.
             " />" 
           else 
             ">" + toHTML(e[2..-1]) + "</" + CGI.escapeHTML(e[0]) + ">"
           end
+      else
+        CGI.escapeHTML(e.to_s)
       end
     end.join("")
   end
